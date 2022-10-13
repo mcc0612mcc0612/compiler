85c69bf6f7cf3466d6815a3d5f4a3e67e647c9cb
vegetarian0fish <3210577619@qq.com>
2022-10-13 22:29:53 +0800

    fun

--- tests/2-ir-gen-warmup/stu_cpp/fun_generator.cpp
+++ tests/2-ir-gen-warmup/stu_cpp/fun_generator.cpp
@@ -24,30 +24,45 @@ int main() {
 
   Type *Int32Type = Type::get_int32_type(module);
 
+  // callee函数
+  // 函数参数类型的vector
+  std::vector<Type *> Ints(1, Int32Type);
+  //通过返回值类型与参数类型列表得到函数类型
+  auto calleeFunTy = FunctionType::get(Int32Type, Ints);
+  // 由函数类型得到函数
+  auto calleeFun = Function::create(calleeFunTy,
+                                 "callee", module);
+
+  auto bb = BasicBlock::create(module, "entry", calleeFun);
+  builder->set_insert_point(bb); 
+
+  auto retAlloca = builder->create_alloca(Int32Type);   // 在内存中分配返回值的位置
+  auto aAlloca = builder->create_alloca(Int32Type);     // 在内存中分配参数u的位置     
+
+  std::vector<Value *> args;  // 获取callee函数的形参,通过Function中的iterator
+  for (auto arg = calleeFun->arg_begin(); arg != calleeFun->arg_end(); arg++) {
+    args.push_back(*arg);   // * 号运算符是从迭代器中取出迭代器当前指向的元素
+  }
+
+  builder->create_store(args[0], aAlloca);  // 将参数a store下来
+  
+  auto aLoad = builder->create_load(aAlloca);
+  auto mul = builder->create_imul(CONST_INT(2), aLoad);  //   a * 2
+  builder->create_store(mul, aAlloca);
+  auto a1Load = builder->create_load(aAlloca);
+  builder->create_ret(a1Load);
+
   // main函数
   auto mainFun = Function::create(FunctionType::get(Int32Type, {}),
                                   "main", module); // 创建并返回函数，参数依次是待创建函数类型 ty，函数名字 name (不可为空)，函数所属的模块 parent
-
-  auto bb = BasicBlock::create(module, "entry", mainFun);
+  bb = BasicBlock::create(module, "entry", mainFun);
 
   // BasicBlock的名字在生成中无所谓,但是可以方便阅读
   builder->set_insert_point(bb);
 
-  auto *arraytype = ArrayType::get(Int32Type, 10);
-  //auto *arraytype = Type::get_array_type(Int32Type,10);
-  auto a_alloca = builder->create_alloca(arraytype);//创建int a[10]
+  auto call = builder->create_call(calleeFun, {CONST_INT(110)});   
 
-
-  auto x0GEP = builder->create_gep(a_alloca, {CONST_INT(0), CONST_INT(0)});  // GEP: 这里为什么是{0, 0}呢? (实验报告相关)
-  builder->create_store(CONST_INT(10), x0GEP);//  a[0] = 10
-
-  auto x0load =builder->create_load(x0GEP);//取出x0的值
-  auto mul = builder->create_imul(CONST_INT(2), x0load);  //   a[0] * 2
-  auto x1GEP = builder->create_gep(a_alloca, {CONST_INT(0), CONST_INT(1)});//获得x1地址
-  builder->create_store(mul, x1GEP);
-  auto x01load =builder->create_load(x1GEP);
-
-  builder->create_ret(x01load);
+  builder->create_ret(call);
   std::cout << module->print();
   delete module;
   return 0;
---
+++ tests/2-ir-gen-warmup/stu_ll/fun_hand.ll
@@ -0,0 +1,28 @@
+; ModuleID = '../c_cases/fun.c'
+source_filename = "../c_cases/fun.c"
+target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-unknown-linux-gnu"
+define i32 @callee(i32 %arg0) {
+label_entry:
+  %op1 = alloca i32
+  %op2 = alloca i32
+  store i32 %arg0, i32* %op2
+  %op3 = load i32, i32* %op2
+  %op4 = mul i32 2, %op3
+  store i32 %op4, i32* %op2
+  %op5 = load i32, i32* %op2
+  ret i32 %op5
+}
+define i32 @main() {
+label_entry:
+  %op0 = call i32 @callee(i32 110)
+  ret i32 %op0
+}
+
+attributes #0 = { noinline nounwind optnone uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "frame-pointer"="all" "less-precise-fpmad"="false" "min-legal-vector-width"="0" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+cx8,+fxsr,+mmx,+sse,+sse2,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.module.flags = !{!0}
+!llvm.ident = !{!1}
+
+!0 = !{i32 1, !"wchar_size", i32 4}
+!1 = !{!"clang version 10.0.1 "}
